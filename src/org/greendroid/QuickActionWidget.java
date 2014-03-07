25c42f1af09c7fd5297f283a04863e32b2647485
LCore <1406754158@qq.com>
2014-03-07 13:58:58 +0800

    方法上做一些优化

--- src/org/greendroid/QuickActionWidget.java
+++ src/org/greendroid/QuickActionWidget.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2010 Cyril Mottier (http://www.cyrilmottier.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.greendroid;
 
 import java.util.ArrayList;
@@ -30,15 +15,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.PopupWindow;
-
 /**
- * Abstraction of a {@link QuickAction} wrapper. A QuickActionWidget is
- * displayed on top of the user interface (it overlaps all UI elements but the
- * notification bar). Clients may listen to user actions using a
- * {@link OnQuickActionClickListener} .
+ * 
+ * @author LCore
+ * 
+ * 自定义对话框
  *
- * @author Benjamin Fellous
- * @author Cyril Mottier
  */
 public abstract class QuickActionWidget extends PopupWindow {
 
@@ -63,28 +45,15 @@ public abstract class QuickActionWidget extends PopupWindow {
 
 	private OnQuickActionClickListener mOnQuickActionClickListener;
 	private ArrayList<QuickAction> mQuickActions = new ArrayList<QuickAction>();
-
-	/**
-	 * Interface that may be used to listen to clicks on quick actions.
-	 *
-	 * @author Benjamin Fellous
-	 * @author Cyril Mottier
-	 */
+    /**
+     * 
+     * @author LCORE
+     *  对话框监听器
+     */
 	public static interface OnQuickActionClickListener {
-		/**
-		 * Clients may implement this method to be notified of a click on a
-		 * particular quick action.
-		 *
-		 * @param position Position of the quick action that have been clicked.
-		 */
 		void onQuickActionClicked(QuickActionWidget widget, int position);
 	}
 
-	/**
-	 * Creates a new QuickActionWidget for the given context.
-	 *
-	 * @param context The context in which the QuickActionWidget is running in
-	 */
 	public QuickActionWidget(Context context) {
 		super(context);
 
@@ -103,12 +72,6 @@ public abstract class QuickActionWidget extends PopupWindow {
 		mScreenHeight = windowManager.getDefaultDisplay().getHeight();
 	}
 
-	/**
-	 * Equivalent to {@link PopupWindow#setContentView(View)} but with a layout
-	 * identifier.
-	 *
-	 * @param layoutId The layout identifier of the view to use.
-	 */
 	public void setContentView(int layoutId) {
 		setContentView(LayoutInflater.from(mContext).inflate(layoutId, null));
 	}
@@ -118,54 +81,22 @@ public abstract class QuickActionWidget extends PopupWindow {
 		mArrowOffsetY = mContext.getResources().getDimensionPixelSize(R.dimen.gd_arrow_offset);
 	}
 
-	/**
-	 * Returns the arrow offset for the Y axis.
-	 *
-	 * @see {@link #setArrowOffsetY(int)}
-	 * @return The arrow offset.
-	 */
 	public int getArrowOffsetY() {
 		return mArrowOffsetY;
 	}
-
-	/**
-	 * Sets the arrow offset to a new value. Setting an arrow offset may be
-	 * particular useful to warn which view the QuickActionWidget is related to.
-	 * By setting a positive offset, the arrow will overlap the view given by
-	 * {@link #show(View)}. The default value is 5dp.
-	 *
-	 * @param offsetY The offset for the Y axis
-	 */
+	
 	public void setArrowOffsetY(int offsetY) {
 		mArrowOffsetY = offsetY;
 	}
-
-	/**
-	 * Returns the width of the screen.
-	 *
-	 * @return The width of the screen
-	 */
+	
 	protected int getScreenWidth() {
 		return mScreenWidth;
 	}
-
-	/**
-	 * Returns the height of the screen.
-	 *
-	 * @return The height of the screen
-	 */
+	
 	protected int getScreenHeight() {
 		return mScreenHeight;
 	}
 
-	/**
-	 * By default, a {@link QuickActionWidget} is dismissed once the user
-	 * clicked on a {@link QuickAction}. This behavior can be changed using this
-	 * method.
-	 *
-	 * @param dismissOnClick True if you want the {@link QuickActionWidget} to
-	 * be dismissed on click else false.
-	 */
 	public void setDismissOnClick(boolean dismissOnClick) {
 		mDismissOnClick = dismissOnClick;
 	}
@@ -181,14 +112,6 @@ public abstract class QuickActionWidget extends PopupWindow {
 		mOnQuickActionClickListener = listener;
 	}
 
-	/**
-	 * Add a new QuickAction to this {@link QuickActionWidget}. Adding a new
-	 * {@link QuickAction} while the {@link QuickActionWidget} is currently
-	 * being shown does nothing. The new {@link QuickAction} will be displayed
-	 * on the next call to {@link #show(View)}.
-	 *
-	 * @param action The new {@link QuickAction} to add
-	 */
 	public void addQuickAction(QuickAction action) {
 		if (action != null) {
 			mQuickActions.add(action);
@@ -196,9 +119,6 @@ public abstract class QuickActionWidget extends PopupWindow {
 		}
 	}
 
-	/**
-	 * Removes all {@link QuickAction} from this {@link QuickActionWidget}.
-	 */
 	public void clearAllQuickActions() {
 		if (!mQuickActions.isEmpty()) {
 			mQuickActions.clear();
@@ -206,12 +126,6 @@ public abstract class QuickActionWidget extends PopupWindow {
 		}
 	}
 
-	/**
-	 * Call that method to display the {@link QuickActionWidget} anchored to the
-	 * given view.
-	 *
-	 * @param anchor The view the {@link QuickActionWidget} will be anchored to.
-	 */
 	public void show(View anchor) {
 
 		final View contentView = getContentView();
@@ -220,9 +134,7 @@ public abstract class QuickActionWidget extends PopupWindow {
 			throw new IllegalStateException("You need to set the content view using the setContentView method");
 		}
 
-		// Replaces the background of the popup with a cleared background
-		setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-
+		setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));//颜色设置为透明
 		final int[] loc = mLocation;
 		anchor.getLocationOnScreen(loc);
 		mRect.set(loc[0], loc[1], loc[0] + anchor.getWidth(), loc[1] + anchor.getHeight());
