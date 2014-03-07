549687811c920586bca67324607d80fcd6e20189
LCore <LCore@10.65.4.72>
2014-03-07 13:32:35 +0800

    RUL工具类中 checkUrl验证细则做略微修改

--- src/org/zirco/utils/UrlUtils.java
+++ src/org/zirco/utils/UrlUtils.java
@@ -23,15 +23,18 @@ import android.content.Context;
 import android.preference.PreferenceManager;
 
 /**
- * Url management utils.
+ * 
+ * @author LCore
+ * @date  2014-03-07
+ *  
+ *    URL工具类
  */
 public class UrlUtils {
-
+    
 	/**
-	 * Check if a string is an url.
-	 * For now, just consider that if a string contains a dot, it is an url.
-	 * @param url The url to check.
-	 * @return True if the string is an url.
+	 * 验证字符串是否为URL
+	 * @param url
+	 * @return
 	 */
 	public static boolean isUrl(String url) {
 		return url.equals(Constants.URL_ABOUT_BLANK) ||
@@ -40,10 +43,10 @@ public class UrlUtils {
 	}
 	
 	/**
-	 * Get the current search url.
-	 * @param context The current context.
-	 * @param searchTerms The terms to search for.
-	 * @return The search url.
+	 * 得到当前需要链接的URL
+	 * @param context
+	 * @param searchTerms
+	 * @return
 	 */
 	public static String getSearchUrl(Context context, String searchTerms) {
 		String currentSearchUrl = PreferenceManager.getDefaultSharedPreferences(context).getString(Constants.PREFERENCES_GENERAL_SEARCH_URL, Constants.URL_SEARCH_GOOGLE);
@@ -51,9 +54,9 @@ public class UrlUtils {
 	}
 	
 	/**
-	 * Check en url. Add http:// before if missing.
-	 * @param url The url to check.
-	 * @return The modified url if necessary.
+	 * 重新包装URL地址(加上http头)
+	 * @param url
+	 * @return string
 	 */
 	public static String checkUrl(String url) {
 		if ((url != null) &&
@@ -74,10 +77,10 @@ public class UrlUtils {
 	}
 	
 	/**
-	 * Check if there is an item in the mobile view url list that match a given url.
-	 * @param context The current context.
-	 * @param url The url to check.
-	 * @return True if an item in the list match the given url.
+	 * 检查设备view url list是否存在给定的URL
+	 * @param context
+	 * @param url
+	 * @return  if exist return true else false
 	 */
 	public static boolean checkInMobileViewUrlList(Context context, String url) {
 		
